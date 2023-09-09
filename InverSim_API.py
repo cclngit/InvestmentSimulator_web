75528b819a873b8c5aacc7cddb71492679b02905
cclngit <38783129+cclngit@users.noreply.github.com>
2023-09-10 01:08:13 +0200

    Flask version

---
+++ InverSim_API.py
@@ -0,0 +1,197 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+class InvestmentSimulator:
+    def __init__(self, duration, inflation_rate, initial_salary, savings_rate, currency="€"):
+        """ This class simulates the evolution of investments over time.
+
+        Args:
+            duration (int): The duration of the simulation in years.
+            inflation_rate (float): The inflation rate in percent.
+            initial_salary (float): The initial salary in euros/usd.
+            savings_rate (float): The percentage of the salary that is saved each month.
+            currency (str): The currency of the simulation.
+        """
+        self.duration = duration
+        self.inflation_rate = inflation_rate / 100
+        self.salary_changes = [(0, initial_salary)] 
+        self.salary = initial_salary
+        self.savings_rate = savings_rate / 100
+        self.investments = []
+        self.currency = currency
+
+    def change_salary(self, year, new_salary):
+        """ Change the salary at a given year.
+        
+        Args:
+            year (int): The year at which the salary is changed.
+            new_salary (float): The new salary in euros/usd.
+        """
+        self.salary_changes.append((year, new_salary))
+        self.salary_changes.sort()
+
+    def get_salary_for_year(self, year):
+        """ Get the salary for a given year.
+        
+        Args:
+            year (int): The year for which the salary is requested.
+        """
+        salary = self.salary_changes[0][1]
+        for change_year, new_salary in self.salary_changes:
+            if change_year > year:
+                break
+            salary = new_salary
+        return salary
+
+    def add_investment(self, name, rate, initial_amount, allocation_percentage, fees=0):
+        """ Add an investment to the simulation.
+        
+        Args:
+            name (str): The name of the investment.
+            rate (float): The nominal rate of the investment in percent.
+            initial_amount (float): The initial amount invested in euros/usd.
+            allocation_percentage (float): The percentage of the salary that is invested each month.
+            fees (float): The fees of the investment in percent.
+        """
+        monthly_amount = self.salary * self.savings_rate * (allocation_percentage / 100)
+        self.investments.append({
+            "name": name,
+            "rate": rate / 100,
+            "initial_amount": initial_amount,
+            "monthly_amount": monthly_amount,
+            "allocation_percentage": allocation_percentage / 100,
+            "fees": fees / 100
+        })
+
+    def calculate_real_rate(self, nominal_rate, fees):
+        """ Calculate the real rate of an investment.
+        
+        Args:
+            nominal_rate (float): The nominal rate of the investment in percent.
+            fees (float): The fees of the investment in percent.
+        """
+        real_rate = ((1 + nominal_rate) / (1 + self.inflation_rate)) - 1
+        return real_rate - fees 
+
+    def simulate_investment(self, investment, duration=None):
+        """ Simulate the evolution of an investment over time.
+        
+        Args:
+            investment (dict): The investment to simulate.
+            duration (int): The duration of the simulation in years.
+        """
+        if duration is None:
+            duration = self.duration
+
+        final_amount = investment["initial_amount"]
+        monthly_amount = self.salary * self.savings_rate * investment["allocation_percentage"]
+
+        for _ in range(duration * 12):
+            salary = self.get_salary_for_year(_ // 12)
+            monthly_amount = salary * self.savings_rate * investment["allocation_percentage"]
+            final_amount += monthly_amount * (1 + investment["rate"] - self.inflation_rate) ** (_ // 12)
+
+        return final_amount
+
+    def simulate(self):
+        """ Simulate the evolution of all investments over time. """
+        results = []
+
+        for investment in self.investments:
+            final_amount = self.simulate_investment(investment)
+            results.append((investment["name"], final_amount))
+
+        return results
+
+    def data_investments(self):
+        """ Save the evolution of all investments over time. """
+        total_final_amounts = [0] * (self.duration + 1)
+
+        for investment in self.investments:
+            final_amounts = []
+            years = list(range(self.duration + 1))
+            for year in years:
+                final_amount = self.simulate_investment(investment, year)
+                final_amounts.append(final_amount)
+                total_final_amounts[year] += final_amount
+
+                salary = self.get_salary_for_year(year)
+                investment["monthly_amount"] = salary * self.savings_rate * investment["allocation_percentage"]
+                
+            # Enregistrement des données dans dict
+            investment["final_amounts"] = final_amounts
+            investment["years"] = years
+           
+        return json.dumps(self.investments)
+        
+@app.route('/simulate', methods=['POST'])
+def simulate_investments():
+    data = request.json
+    print(data)
+    duration = int(data['duration'])
+    inflation_rate = float(data['inflation_rate'])
+    initial_salary = int (data['initial_salary'])
+    savings_rate = int(data['savings_rate'])
+    
+    simulator = InvestmentSimulator(duration=duration, inflation_rate=inflation_rate, initial_salary=initial_salary, savings_rate=savings_rate)
+    
+    investments = data['investments']
+    for investment in investments:
+        name = investment['name']
+        rate = float(investment['rate'])
+        initial_amount = int(investment['initial_amount'])
+        allocation_percentage = int(investment['allocation_percentage'])
+        fees = float(investment.get('fees', 0))
+        simulator.add_investment(name, rate, initial_amount, allocation_percentage, fees)
+
+    salary_changes = data.get('salary_changes', [])
+    for change in salary_changes:
+        year = change['year']
+        new_salary = change['new_salary']
+        simulator.change_salary(year, new_salary)
+
+    results = simulator.simulate()
+    #print(results)
+    return jsonify(results)
+
+@app.route('/plot', methods=['POST'])
+def plot_investments():
+    data = request.json
+    
+    duration = int(data['duration'])
+    inflation_rate = float(data['inflation_rate'])
+    initial_salary = int(data['initial_salary'])
+    savings_rate = int(data['savings_rate'])
+    
+    simulator = InvestmentSimulator(duration=duration, inflation_rate=inflation_rate, initial_salary=initial_salary, savings_rate=savings_rate)
+    
+    investments = data['investments']
+    for investment in investments:
+        name = investment['name']
+        rate = float(investment['rate'])
+        initial_amount = int(investment['initial_amount'])
+        allocation_percentage = int(investment['allocation_percentage'])
+        fees = float(investment.get('fees', 0))
+        simulator.add_investment(name, rate, initial_amount, allocation_percentage, fees)
+
+    salary_changes = data.get('salary_changes', [])
+    for change in salary_changes:
+        year = int(change['year'])
+        new_salary = int(change['new_salary'])
+        simulator.change_salary(year, new_salary)
+    
+    resultats = simulator.data_investments()
+    
+    #with open('data.json', 'w') as outfile:
+     #   outfile.write(resultats)
+        
+    #print(resultats)
+    return resultats
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
---
+++ js/script.js
@@ -0,0 +1,257 @@
+// Fonction pour simuler les investissements
+function simulateInvestments() {
+  // Récupérer les données du formulaire
+  var duration = document.getElementById('duration').value;
+  var inflationRate = document.getElementById('inflationRate').value;
+  var initialSalary = document.getElementById('initialSalary').value;
+  var savingsRate = document.getElementById('savingsRate').value;
+
+  // Créer le corps de la requête
+  var data = {
+    duration: duration,
+    inflation_rate: inflationRate,
+    initial_salary: initialSalary,
+    savings_rate: savingsRate,
+    investments: []
+  };
+
+  // Récupérer les données des investissements
+  var investmentInputs = document.getElementsByClassName('investment-input');
+  for (var i = 0; i < investmentInputs.length; i++) {
+    var investmentInput = investmentInputs[i];
+    var name = investmentInput.getElementsByClassName('name')[0].value;
+    var rate = investmentInput.getElementsByClassName('rate')[0].value;
+    var initialAmount = investmentInput.getElementsByClassName('initial-amount')[0].value;
+    var allocationPercentage = investmentInput.getElementsByClassName('allocation-percentage')[0].value;
+    var fees = investmentInput.getElementsByClassName('fees')[0].value;
+
+    // Ajouter l'investissement à la requête
+    data.investments.push({
+      name: name,
+      rate: rate,
+      initial_amount: initialAmount,
+      allocation_percentage: allocationPercentage,
+      fees: fees
+    });
+  }
+
+  // Effectuer la requête POST à l'API Flask
+  fetch('http://127.0.0.1:5000/simulate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(result => {
+      var resultsContainer = document.getElementById('results');
+      resultsContainer.innerHTML = '';
+      result.forEach(function (investmentResult) {
+        var name = investmentResult[0];
+        var finalAmount = investmentResult[1];
+
+        var card = document.createElement('div');
+        card.classList = "w3-card-4";
+
+        var cardBody = document.createElement('div');
+        cardBody.className = "w3-display-container w3-center w3-ios-dark-blue";
+
+        var nameElement = document.createElement('h2');
+        nameElement.className = "card-title";
+        nameElement.innerHTML = name;
+
+        var finalAmountElement = document.createElement('h3');
+        finalAmountElement.className = "card-text";
+        finalAmountElement.innerHTML = finalAmount.toFixed(2) + ' €';  //toFixed(2) pour arrondir à 2 décimales
+
+        cardBody.appendChild(nameElement);
+        cardBody.appendChild(finalAmountElement);
+        card.appendChild(cardBody);
+        resultsContainer.appendChild(card);
+
+        //Animer les cartes en utilisant les transitions CSS de Bootstrap
+
+        card.classList.add('bg-primary', 'text-white');
+
+        setTimeout(function () {
+          card.classList.remove('bg-primary', 'text-white');
+        }, 1000);
+      });
+    })
+    .catch(error => console.log(error));
+}
+
+function addInvestmentInput() {
+
+  var investmentInputsContainer = document.getElementById('investmentInputs');
+  var investmentInput = document.createElement('div');
+  investmentInput.classList.add('investment-input');
+
+  // Création de la barre horizontale au début
+  var startHr = document.createElement('hr');
+  // Ajout de la classe "my-4" à la barre horizontale
+  startHr.classList.add('my-4');
+  investmentInputsContainer.appendChild(startHr);
+
+  var nameLabel = document.createElement('label');
+  nameLabel.setAttribute('for', 'investmentName');
+  nameLabel.innerText = "Nom de l'investissement :";
+  investmentInput.appendChild(nameLabel);
+
+  var nameInput = document.createElement('input');
+  nameInput.setAttribute('type', 'text');
+  nameInput.classList.add('w3-input', 'name');
+  investmentInput.appendChild(nameInput);
+
+  var rateLabel = document.createElement('label');
+  rateLabel.setAttribute('for', 'investmentRate');
+  rateLabel.innerText = 'Taux de rendement annuel (%) :';
+  investmentInput.appendChild(rateLabel);
+
+  var rateInput = document.createElement('input');
+  rateInput.setAttribute('type', 'number');
+  rateInput.classList.add('w3-input', 'rate');
+  investmentInput.appendChild(rateInput);
+
+  var initialAmountLabel = document.createElement('label');
+  initialAmountLabel.setAttribute('for', 'investmentInitialAmount');
+  initialAmountLabel.innerText = 'Montant initial :';
+  investmentInput.appendChild(initialAmountLabel);
+
+  var initialAmountInput = document.createElement('input');
+  initialAmountInput.setAttribute('type', 'number');
+  initialAmountInput.classList.add('w3-input', 'initial-amount');
+  investmentInput.appendChild(initialAmountInput);
+
+  var allocationPercentageLabel = document.createElement('label');
+  allocationPercentageLabel.setAttribute('for', 'investmentAllocationPercentage');
+  allocationPercentageLabel.innerText = 'Pourcentage d\'allocation (%) :';
+  investmentInput.appendChild(allocationPercentageLabel);
+
+  var allocationPercentageInput = document.createElement('input');
+  allocationPercentageInput.setAttribute('type', 'number');
+  allocationPercentageInput.classList.add('w3-input', 'allocation-percentage');
+  investmentInput.appendChild(allocationPercentageInput);
+
+  var feesLabel = document.createElement('label');
+  feesLabel.setAttribute('for', 'investmentFees');
+  feesLabel.innerText = 'Frais de gestion annuels (%) :';
+  investmentInput.appendChild(feesLabel);
+
+  var feesInput = document.createElement('input');
+  feesInput.setAttribute('type', 'number');
+  feesInput.classList.add('w3-input', 'fees');
+  investmentInput.appendChild(feesInput);
+
+  investmentInputsContainer.appendChild(investmentInput);
+}
+
+// Fonction pour tracer les investissements
+function plotInvestments() {
+  // Récupérer les données du formulaire
+  var duration = document.getElementById('duration').value;
+  var inflationRate = document.getElementById('inflationRate').value;
+  var initialSalary = document.getElementById('initialSalary').value;
+  var savingsRate = document.getElementById('savingsRate').value;
+
+  // Créer le corps de la requête
+  var data = {
+    duration: duration,
+    inflation_rate: inflationRate,
+    initial_salary: initialSalary,
+    savings_rate: savingsRate,
+    investments: []
+  };
+
+  // Récupérer les données des investissements
+  var investmentInputs = document.getElementsByClassName('investment-input');
+  for (var i = 0; i < investmentInputs.length; i++) {
+    var investmentInput = investmentInputs[i];
+    var name = investmentInput.getElementsByClassName('name')[0].value;
+    var rate = investmentInput.getElementsByClassName('rate')[0].value;
+    var initialAmount = investmentInput.getElementsByClassName('initial-amount')[0].value;
+    var allocationPercentage = investmentInput.getElementsByClassName('allocation-percentage')[0].value;
+    var fees = investmentInput.getElementsByClassName('fees')[0].value;
+
+    // Ajouter l'investissement à la requête
+    data.investments.push({
+      name: name,
+      rate: rate,
+      initial_amount: initialAmount,
+      allocation_percentage: allocationPercentage,
+      fees: fees
+    });
+  }
+
+  // Effectuer la requête POST à l'API Flask
+  fetch('http://127.0.0.1:5000/plot', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(result => {
+      // Tracer les données
+      var series = [];
+      var totalData = [];
+
+      result.forEach(function (investment) {
+        var data = investment.years.map(function (year, index) {
+          return {
+            x: year,
+            y: investment.final_amounts[index].toFixed(2) * 1
+          };
+        });
+
+        series.push({
+          name: investment.name,
+          data: data,
+          type: 'line'
+        });
+
+        // Ajouter les données à la somme des investissements
+        if (totalData.length === 0) {
+          totalData = data.map(function (point) {
+            return {
+              x: point.x,
+              //deux chiffres après la virgule
+              y: point.y.toFixed(2) * 1 
+            };
+          });
+        } else {
+          totalData.forEach(function (point, index) {
+            point.y += data[index].y;
+          });
+        }
+      });
+
+      // Ajouter la série de la somme des investissements
+      series.push({
+        name: 'Total',
+        data: totalData,
+        type: 'line'
+      });
+
+      // Créer le graphique avec Highcharts
+      Highcharts.chart('graph-container', {
+        title: {
+          text: 'Investment Simulator'
+        },
+        xAxis: {
+          title: {
+            text: 'Year'
+          }
+        },
+        yAxis: {
+          title: {
+            text: 'Amount'
+          }
+        },
+        series: series
+      });
+    })
+    .catch(error => console.log(error));
+}
